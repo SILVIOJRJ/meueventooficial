15105df89f407759f5c056ae6447616373ee84cc
SILVIOJRJ <wksilviojr@gmail.com>
2024-05-31 15:59:33 -0300

    correção

--- meuevento/lib/main.dart
+++ meuevento/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Eventos App',
+      title: 'meuEvento',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
